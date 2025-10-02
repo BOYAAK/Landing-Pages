3bfe45109ce907700a6e6f58eb81ae7b8b8bff5c
Ansab Ahmed Khan <146860822+BOYAAK@users.noreply.github.com>
2025-10-02 17:43:04 +0500

    Update script.js

--- Itachi Landing Page/script.js
+++ Itachi Landing Page/script.js
@@ -1,3 +1,7 @@
-document.querySelector(".abtnn"),addEventListener("click", function(){
-    var loaDing =  document.querySelector(".abtnn").innerHTML="Loading";
-})
+document.getElementById("readStory").addEventListener("click", function(event){
+    event.preventDefault(); // ⬅️ Stops the page from navigating
+    this.innerHTML = "Loading...";
+    setTimeout(() => {
+        this.innerHTML = "Read Story";
+    }, 2000);
+});
